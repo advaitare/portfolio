ebad7a810e051a7f8a9ce81e822c67e95dde44f9
Advait Tare <advaitare@Advaits-MacBook-Air.local>
2026-03-25 12:01:47 -0500

    fix: add GHA

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "export",
   basePath: "/portfolio",
   assetPrefix: "/portfolio/",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
